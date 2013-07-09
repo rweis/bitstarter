28763a467425927db06d79a03c56e8e2256daa87
rweis <ubuntu@ip-172-31-34-146.us-west-2.compute.internal>
2013-07-09 01:51:47 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
+var fs = require('fs');
+var outfile = "index.html";
+var out = fs.readFileSync(outfile).toString();
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync("index.html").toString());
+  response.send(out);
 });
 
 var port = process.env.PORT || 5000;
